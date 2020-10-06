5381d4e92a28fbdebe61e7c79a7af34f0f20061b
yushin Lin <ysl310607@gmail.com>
2020-10-06 12:00:03 +0800

    修改JSON檔 / 答案比對測試(成功)

--- record.php
+++ record.php
@@ -18,43 +18,63 @@
     
 
     $title = []; // 放入該位使用者的作答紀錄(題號)
+    $usr_ans = [];  // 放入 user【每次測驗】的回答
 
     while($row = mysqli_fetch_array($result , MYSQLI_ASSOC)){ // 一列一列印出資料 (橫列)
 
         // 將資料表中每一列的所有值以 Array 的格式印出
-        print_r( $row);
-        echo "<br><br>";
-       
-        // 印出所選欄位的值 => $row['查詢的欄位1']
-        // echo $row['choosed'] ."<br>";
-        // echo gettype($row['choosed']); // string
+        // print_r($row);
+        // echo "<br><br>";
 
-        $jit = explode(",", $row['choosed']); // 把選中題目(的題號)的字串，轉乘 array
+        $jit = explode("," , $row['choosed']); // 把選中題目(的題號)的字串，轉乘 array
         array_push($title , $jit);
+
+        $tmp = []; // 暫時存放 user【一次測驗】中的回答
+        for ($a = 1; $a < 7; $a++) { // 取出每次測驗中的答案，以測驗次數為一個單位
+            array_push($tmp , $row["num{$a}"]);
+            // print_r($tmp);
+            // echo "<br>";
+        }
+        array_push($usr_ans , $tmp);
 
     }
 
-    echo " title 為: ";
-    print_r($title);
+    echo " usr_ans 為: ";
+    print_r($usr_ans);
     echo "<br><br><br><br>";
 
-    // $str = "Hello,Friend";
-    // $pieces = explode(",", $str); // string to array
-    // print_r($pieces);
 
-    // 使用 PHP 讀取 JSON檔
+    // 使用 PHP 讀取 JSON檔 -- 重要！！！
     $get_Json = file_get_contents("subject.json");
     // Converts to an array 
-    $myarray = json_decode($get_Json, true);
-    var_dump($myarray); // prints array
+    $myarray = json_decode($get_Json , true);
+    // var_dump($myarray); // prints array
 
     echo "<br><br><br>";
     echo " title 陣列長度為: ". sizeof($title) ."<br>";
+    echo " 正解: " . $myarray[0]["right"];  // 印出 JSON 檔中的正解(right)
+    echo "<br>";
+
 
     // 檢查 user 的【答案】是否正確，並寫入各別 user 的狀態表
-    for ($i=0; $i < sizeof($title) ; $i++) { 
-        print_r($title[$i]);
+    for ($i = 0; $i < sizeof($title) ; $i++) { // i 為測驗的次數
+        // print_r($title[$i]);
         echo "<br>";
+
+        for ($j = 0; $j < sizeof($title[$i]) ; $j++) { 
+            echo "<br>";
+            print_r($title[$i][$j]);  // 印出【選擇的題號】
+            echo " => ";
+
+            // 10_06_答案比對(OK)
+            if ($usr_ans[$i][$j] == $myarray[$title[$i][$j]]["right"] ) {
+                # code... 正解 >> $myarray[$title[$i][$j]]["right"]  / 使用者答案 >> $usr_ans[$i][$j]
+                echo "correct";
+            }else{
+                echo "incorrect! {$usr_ans[$i][$j]} != {$myarray[$title[$i][$j]]['right']} ";
+            }
+
+        }
     }
 
 ?>
