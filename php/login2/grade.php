fe85b1465f4dd4d9a1ee35c0a4f745179ab61bb7
yushin Lin <ysl310607@gmail.com>
2020-10-16 21:37:55 +0800

    grade.php 加上外觀(RWD)與登入檢驗機制

--- php/login2/grade.php
+++ php/login2/grade.php
@@ -3,6 +3,9 @@
 
 <?php
 
+    session_start();
+
+
     // 連線
     $host = 'localhost';
     $dbuser = 'root';
@@ -41,13 +44,13 @@
 
         // 取出 使用者 啥時【開始作答】
         array_push($timer , $row['settime']);
-        echo " ROW: ";
-        print_r($row);
-        echo "<br>";
+        // echo " ROW: ";
+        // print_r($row);
+        // echo "<br>";
     }
 
-    echo " timer: ";
-    print_r($timer);
+    // echo " timer: ";
+    // print_r($timer);
 
     // echo " usr【答案 / usr_ans】 為: ";
     // print_r($usr_ans);
@@ -70,21 +73,22 @@
     for ($i = 0; $i < sizeof($title) ; $i++) { // i 為測驗的次數
 
         for ($j = 0; $j < sizeof($title[$i]) ; $j++) { 
-            echo "<br>";
-            print_r($title[$i][$j]);  // 印出【選擇的題號】
-            echo " => ";
+            // echo "<br>";
+            // print_r($title[$i][$j]);  // 印出【選擇的題號】
+            // echo " => ";
+
 
             // user【答案】 與 題庫【JSON檔】 作比對
             // 10_06_答案比對(OK)
             if ($usr_ans[$i][$j] == $myarray[$title[$i][$j]]['right'] ) {
                 # code... 正解 >> $myarray[$title[$i][$j]]["right"]  / 使用者答案 >> $usr_ans[$i][$j]
-                echo "correct";
+                // echo "correct";
             }else{
-                echo "incorrect! {$usr_ans[$i][$j]} != {$myarray[$title[$i][$j]]['right']} ";
+                // echo "incorrect! {$usr_ans[$i][$j]} != {$myarray[$title[$i][$j]]['right']} ";
             }
 
         }
-        echo "<br>";
+        // echo "<br>";
     }
 
 ?>
@@ -96,74 +100,125 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/grade.css">
     <title>股價線圖 -- 新手上路 【學習歷程】</title>
-    <style>
-        table, td, th {
-            border: 1px solid black;
-        }
-    </style>
+    
 </head>
 <body>
-    
-    <?php
 
-        for ($i = 0; $i < sizeof($title) ; $i++) { // i 為測驗的次數
+<?php
+    // 使用 isset() 方法，判別有沒有 is_login 變數可使用，以及為【已登入】
+    if(isset($_SESSION['is_login']) && $_SESSION['is_login']== TRUE):
+?>
 
-            echo "  <p>作答日期與時間：{$timer[$i]}</p>
-                    <table>
-                        <tr>
-                            <th>測驗題順序</th>
-                            <th>所對應之題庫題號</th>
-                            <th>您的回答</th>
-                            <th>正確解答</th>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>{$title[$i][0]}</td>
-                            <td id='usr0'>{$usr_ans[$i][0]}</td>
-                            <td id='ans0'>{$myarray[$title[$i][0]]['right']}</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>{$title[$i][1]}</td>
-                            <td id='usr1'>{$usr_ans[$i][1]}</td>
-                            <td id='ans1'>{$myarray[$title[$i][1]]['right']}</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>{$title[$i][2]}</td>
-                            <td id='usr2'>{$usr_ans[$i][2]}</td>
-                            <td id='ans2'>{$myarray[$title[$i][2]]['right']}</td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>{$title[$i][3]}</td>
-                            <td id='usr3'>{$usr_ans[$i][3]}</td>
-                            <td id='ans3'>{$myarray[$title[$i][3]]['right']}</td>
-                        </tr>
-                        <tr>
-                            <td>5</td>
-                            <td>{$title[$i][4]}</td>
-                            <td id='usr4'>{$usr_ans[$i][4]}</td>
-                            <td id='ans4'>{$myarray[$title[$i][4]]['right']}</td>
-                        </tr>
-                        <tr>
-                            <td>6</td>
-                            <td>{$title[$i][5]}</td>
-                            <td id='usr5'>{$usr_ans[$i][5]}</td>
-                            <td id='ans5'>{$myarray[$title[$i][5]]['right']}</td>
-                        </tr>
-                    </table>";
-            
-            echo "<br><br>";
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+        <a class="navbar-brand mb-0 h1" href="#" style="font-size: 26px;">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Study</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Quiz</a>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Grade</a>
+            </li>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost/KLine/php/login2/logout.php">Logout</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
 
-        }
 
-    ?>
+    <div class="container">
 
-    <!-- 記得補上【登出】 -->
-    <p><a href='http://localhost/KLine/php/login2/logout.php'>登出點我</a></p>
+        <?php
+
+            for ($i = 0; $i < sizeof($title) ; $i++) { // i 為測驗的次數
+                $count = $i + 1;
+
+                echo "<div class='grade'>
+                        <p class='time'>第{$count}次測驗：{$timer[$i]}</p>
+                        <table class='table table-striped'>
+                            <thead>
+                                <tr>
+                                    <th scope='col'>測驗題順序</th>
+                                    <th scope='col'>題庫題號</th>
+                                    <th scope='col'>您的回答</th>
+                                    <th scope='col'>正確解答</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th class='number bg-info' scope='row'>1</th>
+                                <td><a href='#'>{$title[$i][0]}</a></td>
+                                <td>{$usr_ans[$i][0]}</td>
+                                <td>{$myarray[$title[$i][0]]['right']}</td>
+                            </tr>
+
+                            <tr>
+                                <th class='number bg-info' scope='row'>2</th>
+                                <td><a href='#'>{$title[$i][1]}</a></td>
+                                <td>{$usr_ans[$i][1]}</td>
+                                <td>{$myarray[$title[$i][1]]['right']}</td>
+                            </tr>
+
+                            <tr>
+                                <th class='number bg-info' scope='row'>3</th>
+                                <td><a href='#'>{$title[$i][2]}</a></td>
+                                <td>{$usr_ans[$i][2]}</td>
+                                <td>{$myarray[$title[$i][2]]['right']}</td>
+                            </tr>
+
+                            <tr>
+                                <th class='number bg-info' scope='row'>4</th>
+                                <td><a href='#'>{$title[$i][3]}</a></td>
+                                <td>{$usr_ans[$i][3]}</td>
+                                <td>{$myarray[$title[$i][3]]['right']}</td>
+                            </tr>
+
+                            <tr>
+                                <th class='number bg-info' scope='row'>5</th>
+                                <td><a href='#'>{$title[$i][4]}</a></td>
+                                <td>{$usr_ans[$i][4]}</td>
+                                <td>{$myarray[$title[$i][4]]['right']}</td>
+                            </tr>
+
+                            <tr>
+                                <th class='number bg-info' scope='row'>6</th>
+                                <td><a href='#'>{$title[$i][5]}</a></td>
+                                <td>{$usr_ans[$i][5]}</td>
+                                <td>{$myarray[$title[$i][5]]['right']}</td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+                    </div> ";
+                
+            }
+        ?>
+
+    </div>
+
+<?php
+else:
+    // 若使用【非登入帳密】的方法來後台
+    header('Location: http://localhost/KLine/php/login2/login.php');
+
+endif;
+?>
+
 
 <script src="JS/highLight.js"></script> 
 </body>
