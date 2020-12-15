7381f13cab57900040b3a97c321a465150a99b61
yushin Lin <ysl310607@gmail.com>
2020-12-15 21:29:55 +0800

    加入 boostrap css與js檔

--- php/login2/back_exam.php
+++ php/login2/back_exam.php
@@ -9,8 +9,10 @@
     <meta name="author" content="yuShinLin913">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="JS/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="css/exam.css">
     <link rel="icon" href="imgs/favicon.ico" type="image/icon type">
     <title>新手學股票 --【測驗】</title>
